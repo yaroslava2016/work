960b19f7973544507357dff73b7cde1c1c9c1213
yaroslava <yaroslava.svetlichnaya@gmail.com>
2016-02-11 21:47:45 +0200

    arrays loops task

---
+++ arrays_loops_task/1.php
@@ -0,0 +1,5 @@
+<?php
+	$arr=array('html','css','php','js','jq');
+foreach ($arr as $v)
+{echo "$v".'<br>';}
+?>
---
+++ arrays_loops_task/4.php
@@ -0,0 +1,9 @@
+<?php
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+foreach ($arr as $key => $val )
+{echo "$key".'<br>';
+//echo "$val".'<br>';
+}
+foreach ($arr as $key => $val )
+{echo "$val".'<br>';}
+?>
